17af3d251ed7158691109532cdffcd16caa79b42
Hzquel <hzqueltadesse@gmail.com>
2019-10-31 15:35:19 -0700

    bootstrap is added and navigation component is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import Button from 'react-bootstrap/Button';
+// import { Grid, Row, Col } from 'react-bootstrap';
+import Navigation from './components/partials/navbar/navigation'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <h1> Hello Nathan </h1>
       <h2> Welcome to Netlify and React </h2>
+
+
     </div>
   );
 }
